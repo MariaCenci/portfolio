6cb2756171f711cd2c39c6cfa553300e4f56b083
Maria Fernanda Cenci <mariafcenci@gmail.com>
2023-10-18 10:09:17 +0200

    added design home for big screens

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,56 +7,55 @@ const Home: React.FC = () => {
         <div className="container-hero">
           <div className="content-hero">
             <div className="main-hero">
-  </div>
-            <div className="hero-img">
-              <img src="src/images/myPhoto.png" alt="" />
-            </div>
+              <div className="hero-text">
+                <h1>Front-End Developer👩‍💻</h1>
 
-            <div className="hero-text">
-              <h1>
-                Front-End Developer👩‍💻
-          
-              </h1>
+                <p>
+                  Hi, I'm Maria Fernanda Cenci, a Front-End Web Developer based
+                  in Bergamo, Italy.
+                </p>
 
-              <p>
-                Hi, I'm Maria Fernanda Cenci, a Front-End Web Developer based in
-                Bergamo, Italy.
-              </p>
-
-              <span className="social-icons">
-                <a href="https://github.com/MariaCenci">
-                  <img src="src/icons/github.svg" alt="" />
-                </a>
-                <a href="https://www.linkedin.com/in/maria-fernanda-cenci-23a909276/">
-                  <img src="src/icons/linkedin-icon.svg" alt="" />
-                </a>
-              </span>
+                <span className="social-icons">
+                  <a href="https://github.com/MariaCenci">
+                    <img src="src/icons/github.svg" alt="" />
+                  </a>
+                  <a href="https://www.linkedin.com/in/maria-fernanda-cenci-23a909276/">
+                    <img src="src/icons/linkedin-icon.svg" alt="" />
+                  </a>
+                </span>
               </div>
-          
-              <div className="tech-stack">
+
+              <div className="hero-img">
+                <img src="src/images/myPhoto.png" alt="" />
+              </div>
+            </div>
+            <div className="tech-stack">
+
+              <div className="text-stack">
                 <p>Tech Stack</p>
-               <div className="underline"></div>
-               
+              <div className="underline"></div>
+              </div>
+              
+
               <div className="icons-stack">
-                    <span>
-                      <img src="src/icons/html.svg" alt="" />
-                    </span>
-                    <span>
-                      <img src="src/icons/css.svg" alt="" />
-                    </span>
-                    <span>
-                      <img src="src/icons/sass.svg" alt="" />
-                    </span>
-                    <span>
-                    <img src="src/icons/react.svg" alt="" />
-                  </span>
-                  <span>
-                    <img src="src/icons/js.svg" alt="" />
-                  </span>
-                  <span>
-                    <img src="src/icons/ts.svg" alt="" />
-                  </span>
-           
+                <span>
+                  <img src="src/icons/html.svg" alt="" />
+                </span>
+                <span>
+                  <img src="src/icons/css.svg" alt="" />
+                </span>
+                <span>
+                  <img src="src/icons/sass.svg" alt="" />
+                </span>
+                <span>
+                  <img src="src/icons/react.svg" alt="" />
+                </span>
+                <span>
+                  <img src="src/icons/js.svg" alt="" />
+                </span>
+                <span>
+                  <img src="src/icons/ts.svg" alt="" />
+                </span>
               </div>
             </div>
           </div>
